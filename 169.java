2cad7b4a1612dd442aca1b4158e5f0b4d1c7aa53
prithvisaini1222-a11y <prithvisaini1222@gmail.com>
2026-09-08 15:48:32 +0530

    Create 169.java

---
+++ 169.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        HashMap<Integer,Integer> mp = new HashMap<>();
+
+        for(int x : nums){
+            mp.put(x, mp.getOrDefault(x,0)+1);
+        }
+
+        int res = 0;
+
+        for(Map.Entry<Integer,Integer> x : mp.entrySet()){
+            if(x.getValue() > res)
+                res = x.getValue();
+        }
+        for(Map.Entry<Integer,Integer> x : mp.entrySet()){
+            if(x.getValue() == res)
+                return x.getKey();
+        }
+
+        return 0;
+        
+    }
+}
